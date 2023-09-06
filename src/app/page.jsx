a322e39832ce079e9f2120781718c6492b9c9df0
VladislavKuzh <v.s.kuzheliev@gmail.com>
2023-09-06 13:05:26 +0300

    Lesson 4. home, about, contact

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,23 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import Button from '@/components/button/Button';
+import styles from './page.module.css';
+import hero from 'public/hero.png';
 
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <main className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          The Future of AI in the next few years
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the global tech industry.
+        </p>
+        <Button url="/portfolio" text="See Our Works" />
+      </div>
+      <div className={styles.item}>
+        <Image src={hero} alt='main photo' className={styles.img}/>
+      </div>
+    </main>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,17 @@
+import Image from 'next/image';
+
+import styles from './page.module.css';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.container}>
       <div>2023 MyApp. All rights reserved</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image src='/vk.png' width={15} height={15} className={styles.icon} alt="vk.com"/>
+        <Image src="/inst.png" width={15} height={15} className={styles.icon} alt="instagram"/>
+        <Image src="/twitter.png" width={15} height={15} className={styles.icon} alt="twitter"/>
+        <Image src="/yt.png" width={15} height={15} className={styles.icon} alt="youtube"/>
+      </div>
     </footer>
   )
 }
