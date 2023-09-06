a8c8be26eab7f4a2aff4d18146ffb47835a7745b
VladislavKuzh <v.s.kuzheliev@gmail.com>
2023-09-06 18:51:36 +0300

    Lesson 7. fetch data

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,18 +1,29 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-const BlogID = () => {
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const BlogID = async ({params}) => {
+  const data = await getData(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-        <div className={styles.info}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.desc}>
-            desc
-          </p>
+      <div className={styles.info}>
+          <h1 className={styles.title}>{data.title}</h1>
+          <p className={styles.desc}>{data.body}</p>
           <div className={styles.author}>
             <Image
-              src=''
+              src=""
               alt=""
               width={40}
               height={40}
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,19 @@
+'use client'
+
+import useSWR from "swr";
+import styles from "./page.module.css";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+
+  console.log(data);
+
+  return <div className={styles.container}>Dashboard</div>
 }
 
 export default Dashboard
