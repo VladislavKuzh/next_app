a1f96373073cfc673193bd1f9911bfe98e02bf00
VladislavKuzh <v.s.kuzheliev@gmail.com>
2023-09-07 00:34:23 +0300

    Lesson 8. connect mongoDB, create api(post, postID)

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -2,11 +2,12 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 async function getData(id) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
     cache: "no-store",
   });
 
   if (!res.ok) {
+    console.log(res.status)
     throw new Error("Failed to fetch data");
   }
 
@@ -20,21 +21,21 @@ const BlogID = async ({params}) => {
       <div className={styles.top}>
       <div className={styles.info}>
           <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>{data.body}</p>
+          <p className={styles.desc}>{data.desc}</p>
           <div className={styles.author}>
             <Image
-              src=""
+              src={data.img}
               alt=""
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>username</span>
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
           <Image
-            src=''
+            src={data.img}
             alt=""
             fill={true}
             className={styles.image}
@@ -43,7 +44,7 @@ const BlogID = async ({params}) => {
     </div>
     <div className={styles.content}>
       <p className={styles.text}>
-       desc
+       {data.content}
       </p>
     </div>
     </div>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,11 +3,12 @@ import styles from "./page.module.css";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  const res = await fetch("http://localhost:3000/api/posts", {
     cache: "no-store",
   });
 
   if (!res.ok) {
+    console.log(res.status)
     throw new Error("Failed to fetch data");
   }
 
@@ -20,13 +21,13 @@ const Blog = async () => {
     <div className={styles.mainContainer}>
       {data.map((item) => (
         <Link
-          href={`blog/${item.id}`}
+          href={`blog/${item._id}`}
           className={styles.container}
-          key={item.id}
+          key={item._id}
         >
           <div className={styles.imageContainer}>
             <Image
-              src="https://img.freepik.com/free-vector/tiny-house-concept-illustration_114360-9087.jpg?w=826&t=st=1685978770~exp=1685979370~hmac=a60346fbe51d7b8a4713bbfaadc4aaa0365ebec7b120bafb235d0a446efc8676"
+              src={item.img}
               alt=""
               width={400}
               height={250}
@@ -35,7 +36,7 @@ const Blog = async () => {
           </div>
           <div className={styles.content}>
             <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>{item.body}</p>
+            <p className={styles.desc}>{item.desc}</p>
           </div>
         </Link>
       ))}
